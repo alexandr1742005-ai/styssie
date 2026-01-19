5bce5fa60d326a0d05f09f501bc445b1cd979601
Sasha <alexandr.1742005@gmail.com>
2026-01-19 18:49:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
         card.style.animationDelay = `${index * 0.1}s`;
     });
 });
+
+// Поиск по сайту
+const searchInput = document.getElementById('searchInput');
+
+searchInput.addEventListener('input', function() {
+    const searchTerm = this.value.toLowerCase();
+    const cards = document.querySelectorAll('.card');
+    
+    cards.forEach(card => {
+        const cardText = card.textContent.toLowerCase();
+        
+        if (cardText.includes(searchTerm)) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
